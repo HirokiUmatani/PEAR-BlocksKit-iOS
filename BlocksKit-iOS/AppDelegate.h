aae0d5bb11564f43c5685d9bbb614d2feffa7509
HirokiUmatani <o9093009555@gmail.com>
2015-11-01 18:39:49 +0900

    update project file

---
+++ BlocksKit-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BlocksKit-iOS
+//
+//  Created by hirokiumatani on 2015/11/01.
+//  Copyright © 2015年 hirokiumatani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BlocksKit-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BlocksKit-iOS
+//
+//  Created by hirokiumatani on 2015/11/01.
+//  Copyright © 2015年 hirokiumatani. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
